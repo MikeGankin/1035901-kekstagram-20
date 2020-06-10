3f2444eb2659e12c1484ca4d5fdf6616564b2bd2
Михаил <mikhail-gankin@yandex.ru>
2020-06-10 18:06:12 +0300

    Fix card render

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var comments = [
+var Comments = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -9,52 +9,62 @@ var comments = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
-var names = [
+var Names = [
   'Миша', 'Андрей', 'Дима', 'Никита', 'Таня', 'Тема'
 ];
 
 // Случайное число от и до
-var randomInteger = function (min, max) {
-  var rand = min + Math.random() * (max + 1 - min);
-  return Math.floor(rand);
+var getRandomInteger = function (min, max) {
+  var randNumber = min + Math.random() * (max + 1 - min);
+  return Math.floor(randNumber);
 };
 
 // Случайный элемент массива
-function arrayRandElement(arr) {
-  var rand = Math.floor(Math.random() * arr.length);
-  return arr[rand];
-}
+var getArrayRandElement = function (arr) {
+  var randElement = Math.floor(Math.random() * arr.length);
+  return arr[randElement];
+};
 
-// Генерируем объекты с комментариями
+// Генерируем объекты с комментариями к фотографиям
+var generateComments = function () {
+  var comments = [];
+  for (var i = 0; i < getRandomInteger(1, 5); i++) {
+    var comment = {
+      avatar: 'img/avatar-' + getRandomInteger(1, 6) + '.svg',
+      message: getArrayRandElement(Comments),
+      name: getArrayRandElement(Names)
+    };
+    comments.push(comment);
+  }
+  return comments;
+};
+
+// Генерируем объекты с фотографиям и добавляем комментарии
 var generateData = function () {
   var data = [];
-  var comment = {
-    avatar: 'img/avatar-' + randomInteger(1, 6) + '.svg',
-    message: arrayRandElement(comments),
-    name: arrayRandElement(names)
-  };
-
   for (var i = 0; i < 25; i++) {
     var post = {
-      url: 'photos/' + randomInteger(1, 25) + '.jpg',
+      url: 'photos/' + (i + 1) + '.jpg',
       description: 'Вот так надо фоткать!',
-      likes: randomInteger(15, 200),
-      comments: comment
+      likes: getRandomInteger(15, 200),
+      comments: generateComments()
     };
     data.push(post);
   }
+
+  return data;
 };
-generateData();
 
 // Рендерим карточки на страницу
-var renderCards = function () {
+var renderCards = function (data) {
   var pictures = document.querySelector('.pictures section');
-  var template = document.querySelector('#picture').content.querySelector('a');
-
-  for (var i = 1; i < 25; i++) {
+  var template = document.querySelector('#picture').content.querySelector('.picture');
+  for (var i = 0; i < 25; i++) {
     var element = template.cloneNode(true);
-    element.querySelector('.picture__img').src = 'photos/' + i + '.jpg';
+    element.querySelector('.picture__img').src = data[i].url;
+    element.querySelector('.picture__comments').textContent = data[i].comments.length;
+    element.querySelector('.picture__likes').textContent = data[i].likes;
     pictures.insertAdjacentElement('beforebegin', element);
   }
 };
-renderCards();
+renderCards(generateData());
