351ffa9e0a1961c0bf6dd1eea424a17c59baf536
Михаил <mikhail-gankin@yandex.ru>
2020-07-28 19:39:43 +0300

    checkServerResponse

--- js/server.js
+++ js/server.js
@@ -7,8 +7,7 @@
   };
   var TIMEOUT_IN_MS = 10000;
 
-  var load = function (onSuccess, onError) {
-    var xhr = new XMLHttpRequest();
+  var checkServerResponse = function (xhr, onSuccess, onError) {
     xhr.responseType = 'json';
 
     xhr.addEventListener('load', function () {
@@ -22,6 +21,12 @@
     xhr.addEventListener('error', function () {
       onError('Произошла ошибка соединения');
     });
+  };
+
+  var load = function (onSuccess, onError) {
+    var xhr = new XMLHttpRequest();
+
+    checkServerResponse(xhr, onSuccess, onError);
 
     xhr.addEventListener('timeout', function () {
       onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
@@ -35,19 +40,8 @@
 
   var upload = function (data, onSuccess, onError) {
     var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
 
-    xhr.addEventListener('load', function () {
-      if (xhr.status === StatusCode.OK) {
-        onSuccess(xhr.response);
-      } else {
-        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    });
-
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
+    checkServerResponse(xhr, onSuccess, onError);
 
     xhr.open('POST', URL);
     xhr.send(data);
